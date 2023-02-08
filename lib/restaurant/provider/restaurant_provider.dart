5f730aeb287fbb764501da739c910bdf321bfef3
fenrir <finrir@matey.co.kr>
2023-02-08 09:45:35 +0900

    pagination 완료

--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -31,67 +31,87 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
     // 강제 재로딩. true: CursorPaginationLoading(),
     bool forceRefetch = false,
   }) async {
-    // final resp = await repository.paginate();
-    // state = resp;
-    // State의 5가지 가능성
-    // 1) CursorPagination: 정상적인 데이터가 있는 상태
-    // 2) CursorPaginationLoading: 데이터가 로딩중인 상태(현재 캐시 없음)
-    // 3) CursorPaginationError: 에러 발생한 상태
-    // 4) CursorPaginationRefetching: 첫번째 페이지부터 재요청 상태
-    // 5) CursorPaginationFetchMore: 추가 데이터를 paginagtion 요청한 상태
+    try {
+      // final resp = await repository.paginate();
+      // state = resp;
+      // State의 5가지 가능성
+      // 1) CursorPagination: 정상적인 데이터가 있는 상태
+      // 2) CursorPaginationLoading: 데이터가 로딩중인 상태(현재 캐시 없음)
+      // 3) CursorPaginationError: 에러 발생한 상태
+      // 4) CursorPaginationRefetching: 첫번째 페이지부터 재요청 상태
+      // 5) CursorPaginationFetchMore: 추가 데이터를 paginagtion 요청한 상태
 
-    // * 바로 반환하는 상태
-    // 1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 요소)
-    // 2) 로딩중: fetchMore == true
-    //           fetchMore == false: 새로 고침의 의도가 있을 수 있다.
-    if (state is CursorPagination && !forceRefetch) {
-      final pState = state as CursorPagination;
-      if (!pState.meta.hasMore) {
+      // * 바로 반환하는 상태
+      // 1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 요소)
+      // 2) 로딩중: fetchMore == true
+      //           fetchMore == false: 새로 고침의 의도가 있을 수 있다.
+      if (state is CursorPagination && !forceRefetch) {
+        final pState = state as CursorPagination;
+        if (!pState.meta.hasMore) {
+          return;
+        }
+      }
+
+      final isLoading = state is CursorPaginationLoading;
+      final isRefetching = state is CursorPaginationRefetching;
+      final isFetchingMore = state is CursorPaginationFetchingMore;
+
+      if (fetchMore && (isLoading || isRefetching || isFetchingMore)) {
         return;
       }
-    }
 
-    final isLoading = state is CursorPaginationLoading;
-    final isRefetching = state is CursorPaginationRefetching;
-    final isFetchingMore = state is CursorPaginationFetchingMore;
-
-    if (fetchMore && (isLoading || isRefetching || isFetchingMore)) {
-      return;
-    }
-
-    // PaginationParams 생성
-    PaginationParams paginationParams = PaginationParams(
-      count: fetchCount,
-    );
-
-    // fetchMore: 데이터를 추가로 가져오는 상황
-    if (fetchMore) {
-      final pState = state as CursorPagination;
-
-      state = CursorPaginationFetchingMore(
-        meta: pState.meta,
-        data: pState.data,
+      // PaginationParams 생성
+      PaginationParams paginationParams = PaginationParams(
+        count: fetchCount,
       );
 
-      paginationParams = paginationParams.copyWith(
-        after: pState.data.last.id,
+      // fetchMore: 데이터를 추가로 가져오는 상황
+      if (fetchMore) {
+        final pState = state as CursorPagination;
+
+        state = CursorPaginationFetchingMore(
+          meta: pState.meta,
+          data: pState.data,
+        );
+
+        paginationParams = paginationParams.copyWith(
+          after: pState.data.last.id,
+        );
+      } else {
+        // 데이터를 처음부터 가져오는 상황
+        // 만약에 데이터가 있는 상황이라면 기존 데이터에 Fetch 진행
+        if (state is CursorPagination && !forceRefetch) {
+          final pState = state as CursorPagination;
+
+          state = CursorPaginationRefetching(
+            meta: pState.meta,
+            data: pState.data,
+          );
+          // 기존 데이터를 유지할 필요가 없는 상황
+        } else {
+          state = CursorPaginationLoading();
+        }
+      }
+
+      final resp = await repository.paginate(
+        paginationParams: paginationParams,
       );
-    }
 
-    final resp = await repository.paginate(
-      paginationParams: paginationParams,
-    );
+      if (state is CursorPaginationFetchingMore) {
+        final pState = state as CursorPaginationFetchingMore;
 
-    if (state is CursorPaginationFetchingMore) {
-      final pState = state as CursorPaginationFetchingMore;
-
-      // 기존 데이터에 새로운 데이터 추가
-      state = resp.copyWith(
-        data: [
-          ...pState.data,
-          ...resp.data,
-        ],
-      );
+        // 기존 데이터에 새로운 데이터 추가
+        state = resp.copyWith(
+          data: [
+            ...pState.data,
+            ...resp.data,
+          ],
+        );
+      } else {
+        state = resp;
+      }
+    } catch(e) {
+      state = CursorPaginationError(message: '데이터를 가져오지 못했습니다.');
     }
   }
 }
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,25 +1,47 @@
-import 'package:actual/common/const/data.dart';
-import 'package:actual/common/dio/dio.dart';
 import 'package:actual/common/model/cursor_pagination_model.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
-import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:actual/restaurant/provider/restaurant_provider.dart';
-import 'package:actual/restaurant/repository/restaurant_repository.dart';
 import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    controller.addListener(scrollListner);
+
+    super.initState();
+  }
+
+  void scrollListner() {
+    // 현재 위치가 최대길이보다 조금 못미치는 위치라면 새로운 데이터 요청
+    if (controller.offset > controller.position.maxScrollExtent - 300) {
+      ref.read(restaurantProvider.notifier).paginate(
+        fetchMore: true,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
     if (data is CursorPaginationLoading) {
       return const Center(
         child: CircularProgressIndicator(),
+      );
+    } else if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
       );
     }
 
@@ -30,8 +52,23 @@ class RestaurantScreen extends ConsumerWidget {
         horizontal: 16.0,
       ),
       child: ListView.separated(
-        itemCount: cp.data.length,
+        controller: controller,
+        itemCount: cp.data.length + 1,
         itemBuilder: (_, index) {
+          if (index == cp.data.length) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 8.0,
+              ),
+              child: Center(
+                child: data is CursorPaginationFetchingMore
+                  ? const CircularProgressIndicator()
+                  : const Text('마지막 데이터입니다.'),
+              ),
+            );
+          }
+
           final pItem = cp.data[index];
           // final pItem = RestaurantModel.fromJson(item);
 
